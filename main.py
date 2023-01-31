f6ca78e8f2abceb84ceecc0fa28719f5c9c6f6ae
lzy1102 <575566430@qq.com>
2023-01-31 17:28:33 +0800

    init

--- main.py
+++ main.py
@@ -1,73 +1,26 @@
 import network
 import socket
-import _thread
 import time
-from machine import Pin, Timer
-import btree
 import ustruct
 from urequests import urequests
-import wifimgr  # 这个模块名称务必要与上述WiFiManager模块文件名一致
-
-# wlan = wifimgr.get_connection()
-# if wlan is None:
-#     print("Could not initialize the network connection.")
-#     while True:
-#         pass  # you shall not pass :D
-#
-#
-# # Main Code goes here, wlan is a working network.WLAN(STA_IF) instance.
-# print("ESP OK")
-
-LED_BLINK = True
 
 
-def led():
-    p2 = Pin(12, Pin.OUT)
-    p3 = Pin(13, Pin.OUT)
-    while True:
-        if not LED_BLINK:
-            p2.off()
-            p3.off()
-            time.sleep(1000)
-            continue
-        p2.on()
-        p3.off()
-        time.sleep_ms(500)
-        p2.off()
-        p3.on()
-        time.sleep_ms(500)
-
-
-def serverWifi():
-    # wlan = wifimgr.get_connection()
-    # if wlan is None:
-    #     print("Could not initialize the network connection.")
-    #     while True:
-    #         pass  # you shall not pass :D
-    print("启动wifi server")
-    ap = network.WLAN(network.AP_IF)  # create access-point interface
-    ap.config(essid='ESP-AP', authmode=network.AUTH_WPA_WPA2_PSK, channel=11, password="123456789",
-              max_clients=10)  # set the SSID of the access point
-    ap.active(True)  # activate the interface
-    print("启动wifi server 完成", ap.ifconfig())
-
-
-def connectWifi():
+def connectWifi(ssid, password):
     wlan = network.WLAN(network.STA_IF)  # create station interface
     wlan.active(True)  # activate the interface
     wlan.disconnect()
-    wlan.connect('yangdabao', 'lizhiyong418')  # connect to an AP
-    time.sleep(5)
+    wlan.connect(ssid, password)  # connect to an AP
+    time.sleep(5)  # 等待几秒，要不然isconnected 一直是false
     if wlan.isconnected():
         print(wlan.ifconfig())  # get the interface's IP/netmask/gw/DNS addresses
 
 
-def wake_up(mac='DC-4A-3E-78-3E-0A'):
-    MAC = mac
-    BROADCAST = "192.168.3.255"
-    if len(MAC) != 17:
+def wake_up(mac='DC-4A-3E-78-3E-0A', broadcast="192.168.1.255", port=7):
+    # MAC = mac
+    # BROADCAST = "192.168.3.255"
+    if len(mac) != 17:
         raise ValueError("MAC address should be set as form 'XX-XX-XX-XX-XX-XX'")
-    mac_address = MAC.replace("-", '')
+    mac_address = mac.replace("-", '')
     data = ''.join(['FFFFFFFFFFFF', mac_address * 20])  # 构造原始数据格式
     send_data = b''
 
@@ -80,11 +33,11 @@ def wake_up(mac='DC-4A-3E-78-3E-0A'):
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.sendto(send_data, (BROADCAST, 9))
+        sock.sendto(send_data, (broadcast, port))
         time.sleep(1)
-        sock.sendto(send_data, (BROADCAST, 9))
+        sock.sendto(send_data, (broadcast, port))
         time.sleep(1)
-        sock.sendto(send_data, (BROADCAST, 9))
+        sock.sendto(send_data, (broadcast, port))
         sock.close()
         print("Done")
     except Exception as e:
@@ -93,38 +46,27 @@ def wake_up(mac='DC-4A-3E-78-3E-0A'):
 
 
 def main():
-    global LED_BLINK
-    print('----所有线程开始执行----')
-    # serverWifi()
-    # 创建互斥锁
-    gLock = _thread.allocate_lock()
-    # 获得互斥锁
-    gLock.acquire()
-    # 创建线程 点亮led灯
-    _thread.start_new_thread(led, ())
-    # 休眠
-    time.sleep(5)
-    # 释放互斥锁
-    gLock.release()
-    print('----主程序正在执行----')
-    connectWifi()
+    ssid = "xxx"
+    password = "xxx"
+    mac = "E0-D5-5E-7D-B9-EA"
+    broadcast = "192.168.3.255"  # 广播地址
+    port = 7  # 7 或者 9
+    api = "http://api.xinjing123.top:5000"
+    key = "***"
+    connectWifi(ssid=ssid, password=password)
     while True:
-        time.sleep_ms(20000)
-        print("循环开始")
         try:
-            resp = urequests.get("http://106.12.129.198:5000/get")
+            resp = urequests.get(api + "/get/status", params={"mac": mac, "key": key})
             print(resp.status_code)
             if str(resp.status_code) != "200":
                 continue
             if resp.text == "1":
-                LED_BLINK=True
                 print("开机")
-                wake_up(mac="E0-D5-5E-7D-B9-EA")
-                time.sleep_ms(5000)
-                urequests.get("http://106.12.129.198:5000/set?status=0")
+                wake_up(mac=mac, broadcast=broadcast, port=port)
+                time.sleep_ms(10000)
+                urequests.get(api + "/set/status", params={"mac": mac, "key": key, "status": 0})
         except Exception as e:
             print("获取状态错误", e)
-        LED_BLINK=False
 
 
 if __name__ == '__main__':
--- pcserver.py
+++ pcserver.py
@@ -1,24 +1,98 @@
+import json
+import random
+import re
+import time
 from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+import hashlib
 
+# create the extension
+db = SQLAlchemy()
+# create the app
 app = Flask(__name__)
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///startup.db"
+# initialize the app with the extension
+db.init_app(app)
 
 
-@app.route('/set')
+class StartUp(db.Model):
+    __tablename__ = "start_up"  # 指定表名称
+    id = db.Column(db.Integer, primary_key=True)
+    mac = db.Column(db.String, unique=True, nullable=False)
+    key = db.Column(db.String)
+    status = db.Column(db.String)
+
+
+with app.app_context():
+    db.create_all()
+
+
+def validate_mac(value):
+    if value.find('-') != -1:
+        pattern = re.compile(r"^\s*([0-9a-fA-F]{2,2}-){5,5}[0-9a-fA-F]{2,2}\s*$")
+        if pattern.match(value):
+            return True
+        else:
+            return False
+    if value.find(':') != -1:
+        pattern = re.compile(r"^\s*([0-9a-fA-F]{2,2}:){5,5}[0-9a-fA-F]{2,2}\s*$")
+        if pattern.match(value):
+            return True
+        else:
+            return False
+
+
+@app.route('/set/status', methods=["GET"])
 def set_status():
     status = request.args.get("status")
-    with open("status.txt","w+") as f:
-        f.write(status)
-    return "ok"
+    key = request.args.get("key")
+    mac = request.args.get("mac")
+    if mac == "" or mac is None or validate_mac(mac) is False:
+        return "mac is null"
+    if status != "0" and status != "1":
+        return "status is 0 or 1"
+    if key == "" or key is None:
+        return "key is null"
+    tmp = StartUp.query.filter_by(mac=mac, key=key).first()
+    if tmp is None:
+        return "not mac"
+    else:
+        tmp.status = status
+        db.session.commit()
+        return tmp.status
 
-@app.route("/get")
+
+@app.route('/get/status', methods=["GET"])
 def get_status():
-    try:
-        with open("status.txt","r+") as f:
-            data=f.read()
-        return data
-    except:
-        return "0"
+    key = request.args.get("key")
+    mac = request.args.get("mac")
+    if mac == "" or mac is None or validate_mac(mac) is False:
+        return "mac is null"
+    if key == "" or key is None:
+        return "key is null"
+    tmp = StartUp.query.filter_by(mac=mac, key=key).first()
+    if tmp is None:
+        return "not mac"
+    else:
+        return tmp.status
+
+
+@app.route("/get/key", methods=["GET"])
+def get_key():
+    mac = request.args.get("mac")
+    if mac == "" or mac is None or validate_mac(mac) is False:
+        return "mac is null"
+    tmp = StartUp.query.filter_by(mac=mac).first()
+    if tmp is None:
+        shark=str(random.random())
+        result = hashlib.md5(str(mac + shark).encode()).hexdigest()
+        db.session.add(StartUp(mac=mac, key=result, status="0"))
+        db.session.commit()
+        return str(result)
+    else:
+        return tmp.key
 
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(port=5000, host="0.0.0.0", debug=False)
