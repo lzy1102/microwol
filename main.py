a2175b1032b2f7bedf434c4b74bdf65511dc6d03
lzy1102 <575566430@qq.com>
2023-01-31 17:45:57 +0800

    init

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def main():
     key = "583a80ba213d9361891994488f7715e8"
     connectWifi(ssid=ssid, password=password)
     while True:
-        time.sleep(10000)
+        time.sleep(10)
         try:
             resp = urequests.get(api + "/get/status", params={"mac": mac, "key": key})
             print(resp.status_code)
@@ -62,7 +62,7 @@ def main():
             if resp.text == "1":
                 print("开机")
                 wake_up(mac=mac, broadcast=broadcast, port=port)
-                time.sleep(10000)
+                time.sleep(10)
                 urequests.get(api + "/set/status", params={"mac": mac, "key": key, "status": 0})
         except Exception as e:
             print("获取状态错误", e)
