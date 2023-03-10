e9327af18bc68de94014fa81a14ba105d09cdbd1
lzy1102 <575566430@qq.com>
2023-03-10 14:09:19 +0800

    chatgpt 登录不了

--- chatgpt/ai/apiapi.py
+++ chatgpt/ai/apiapi.py
@@ -9,16 +9,16 @@ from ai.status import WorkingStatus, WorkingType
 
 class ApiAI(AiBase):
 
-    def __init__(self, key):
+    def __init__(self, organization, key):
         super().__init__()
-        print("token",key)
+        print("token", key)
         self.type = WorkingType.Api
         self.key = key
+        openai.organization = organization
         openai.api_key = key
         # list engines
-        engines = openai.Engine.list()
-        # print the first engine's id
-        # print(engines)
+        # engines = openai.Engine.list()
+        # Note: you need to be using OpenAI Python v0.27.0 for the code below to work
 
     def send(self, msg):
         super().send(msg=msg)
@@ -30,8 +30,19 @@ class ApiAI(AiBase):
         try:
             # text-davinci-003
             # text-davinci-002-render
-            completion = openai.Completion.create(engine="text-davinci-003", prompt=msg, max_tokens=4000,
-                                                  temperature=0.5)
+            completion = openai.ChatCompletion.create(
+                api_key=self.key,
+                model="gpt-3.5-turbo",
+                messages=[
+                    # {"role": "system", "content": "You are a helpful assistant."},
+                    # {"role": "user", "content": "Who won the world series in 2020?"},
+                    # {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+                    {"role": "user", "content": msg}
+                ]
+            )
+            return {'id': completion['id'], "message": completion.choices[0].message.content}
+            # completion = openai.Completion.create(engine="text-davinci-003", prompt=msg, max_tokens=4000,
+            #                                       temperature=0.5)
             # print the completion
             # with open("completion.json", mode="w+", encoding="utf-8") as f:
             #     f.write(json.dumps(completion))
@@ -45,7 +56,7 @@ class ApiAI(AiBase):
 
 
 if __name__ == '__main__':
-    aai = ApiAI(key="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")
+    aai = ApiAI(organization="", key="")
     print(aai.key)
     print(aai.status)
     print(aai.send(msg="如何提高工作效率用中文回答"))
